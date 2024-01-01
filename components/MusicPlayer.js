06cc7290badfcf8066f15a128dff9035016ddc34
Brilliano Dhiya Ulhaq <brillidhiya@gmail.com>
2024-01-01 23:40:01 +0700

    cobain lagi ya ada attention

--- components/MusicPlayer.js
+++ components/MusicPlayer.js
@@ -9,6 +9,8 @@ import { useInteraction } from "./UseInteraction";
 function MusicPlayer() {
   // Ref untuk audio element
   const audioRef = useRef(null);
+  // State untuk menyimpan status modal
+  const [showModal, setShowModal] = useState(true);
 
   // State untuk menyimpan status pemutaran
   const [playing, setPlaying] = useState(false);
@@ -36,17 +38,106 @@ function MusicPlayer() {
   // Memanggil custom hook dan mendapatkan status interaksi user
   const userInteracted = useInteraction();
 
-  useEffect(() => {
-    if (!playing && userInteracted) {
-      setTimeout(() => {
-        togglePlay();
-      }, 2000);
-    }
-    return () => {};
-  }, [userInteracted]);
+  //   useEffect(() => {
+  //     if (!playing && userInteracted) {
+  //       setTimeout(() => {
+  //         togglePlay();
+  //       }, 2000);
+  //     }
+  //     return () => {};
+  //   }, [userInteracted]);
 
+  // Fungsi untuk menyembunyikan modal
+  const closeModal = () => {
+    setShowModal(false);
+    togglePlay();
+  };
+
+  // Fungsi untuk menangani klik luar modal
+  const handleClickOutside = (event) => {
+    // Jika target klik adalah elemen dengan id modal-overlay, tutup modal
+    if (event.target.id === "modal-overlay") {
+      closeModal();
+      togglePlay();
+    }
+  };
   return (
     <div className="absolute z-20 bg-white w-[18vw] h-[17vw] bottom-[1.5vw] right-[1.5vw] radius-white">
+      {showModal && (
+        <div
+          id="modal-overlay"
+          className="z-30 fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleClickOutside}
+        >
+          <div className="w-[40vw] p-[1vw] bg-white rounded shadow-lg">
+            <div className="flex items-center justify-between">
+              <h3 className="text-[2vw] font-bold">Attention!</h3>
+              <button
+                className="p-[0.2vw] text-[2vw] text-gray-400 hover:text-gray-600"
+                onClick={closeModal}
+              >
+                x
+              </button>
+            </div>
+            <div className="mt-[0.8vw]">
+              <p className="text-[1.2vw] text-left">
+                Project ini hanyalah sebatas hasil hobi bukan untuk commercial.{" "}
+                <Image
+                  alt="kobo-tokped2"
+                  className="float-right mr-[2vw] mt-[1.8vw]"
+                  src={"/GBJScPKaAAAvqx-.jpg"}
+                  width={240}
+                  height={240}
+                />
+              </p>
+              {/* <Image
+                alt="kobo-tokped2"
+                className="float-right absolute"
+                src={"/GBJScPLbwAABSPt.jpg"}
+                width={200}
+                height={200}
+              /> */}
+              <p className="mt-[0.5vw] text-[1.2vw]"># Illustration By</p>
+              <ul className="list-disc list-inside">
+                <li className="">
+                  <a
+                    className="text-blue-600"
+                    target="_blank"
+                    href="https://twitter.com/anzailee1"
+                  >
+                    @anzailee1
+                  </a>
+                </li>
+                <li className="">
+                  <a
+                    className="text-blue-600"
+                    target="_blank"
+                    href="https://twitter.com/keenbiscuit"
+                  >
+                    @keenbiscuit
+                  </a>
+                </li>
+              </ul>
+              <p className="mt-[0.5vw] text-[1.2vw]"># Design By</p>
+              <ul className="list-disc list-inside">
+                <li className="">
+                  <a
+                    className="text-blue-600"
+                    target="_blank"
+                    href="https://www.instagram.com/lilia_sekai/"
+                  >
+                    @lilia_sekai{" "}
+                  </a>
+                </li>
+              </ul>
+              <p className="mt-[0.5vw] text-[1.2vw]"># Develop By</p>
+              <ul className="list-disc list-inside">
+                <li className="">@kanrisha</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
       {/* Audio element dengan autoplay dan loop */}
       <audio
         ref={audioRef}
